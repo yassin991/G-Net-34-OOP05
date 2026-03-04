6c46748250f892a4a1d36c7b984a0b2a0596230c
yassin991 <yassenelsherbiny69@gmail.com>
2026-03-04 16:17:48 +0200

    Q1

--- AssignmentOOP05/Program.cs
+++ AssignmentOOP05/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace AssignmentOOP05
+﻿using System.ComponentModel;
+using System.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AssignmentOOP05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
-        }
+        #region Q1
+//        Q1: What is an interface in C#?  
+//            An interface in C# is a reference type that defines a contract.
+//It contains method signatures, properties, events, or indexers without implementation(by default).
+//A class that implements an interface must provide the implementation for all its members.
+//            Why do we use interfaces instead of depending on concrete classes directly?
+//                     We use interfaces to achieve loose coupling.
+//               At least Three Benefits of Using Interfaces
+//                  1.Loose Coupling
+//                  2.Polymorphism
+//                  3.Easier Testing 
+    #endregion
+
+
+    }
     }
 }
